7a640613a44519b850762b3caeb4e1cae3930f8f
Timotius-Nugroho <timotiusnugroho999@gmail.com>
2021-06-04 22:59:19 +0700

    first commit

---
+++ next.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    APP_NAME: "DompetMu",
+    BASE_URL: "http://localhost:3004/backend4/api/v1",
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/login", // sebagai pengganti path
+        destination: "/auth/login", // sebagai lokasi asli
+      },
+    ];
+  },
+};
